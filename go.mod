ad6bb7cc4f412e1f307f8f653085fbafe5e0bea2
james <james.min@freedgrouptech.com>
2022-01-10 19:14:37 +0900

    feat: use env for go

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/djmin43/marriage-invitation-back-end
 
 go 1.17
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
 import (
+	// "database/sql"
+	// "fmt"
+
 	"fmt"
-	"net/http"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+const (
+	// Initialize connection constants.
+	HOST     = "marriage-invitation.postgres.database.azure.com"
+	DATABASE = "postgres"
+	USER     = "mindongjoon"
+	PASSWORD = "<server_admin_password>"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getPassword() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	return port
+}
+
 func main() {
-	fmt.Println("hello")
-	http.ListenAndServe(":5000", nil)
+	dbPassword := getPassword()
+	fmt.Println(dbPassword)
 }
