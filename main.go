6d93373a8a36a58d2b253e2e24b38aa1ad69ef26
james <james.min@freedgrouptech.com>
2022-01-12 18:11:13 +0900

    feat: get data from azure db portal

--- main.go
+++ main.go
@@ -31,6 +31,27 @@ func connectToDB() {
 	err = db.Ping()
 	checkError(err)
 	fmt.Println("connection successful!")
+
+	var id string
+	var name string
+	var email string
+
+	sql_statement := `SELECT * from main."user"`
+	rows, err := db.Query(sql_statement)
+	checkError(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		switch err := rows.Scan(&id, &name, &email); err {
+		case sql.ErrNoRows:
+			fmt.Println("No rows were returned")
+		case nil:
+			fmt.Printf("Data row = (%s, %s, %s)\n", id, name, email)
+		default:
+			checkError(err)
+		}
+	}
+
 }
 
 func checkError(err error) {
