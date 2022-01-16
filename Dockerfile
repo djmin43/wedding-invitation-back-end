30f0b7d7309a580a1e02c41e622267ef138bf84c
james <james.min@freedgrouptech.com>
2022-01-17 07:26:01 +0900

    feat: create a working dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,21 @@
-#!/usr/bin/env bash
-FROM golang:latest
+#!/usr/bin
+FROM golang:1.17-buster as builder
+
+WORKDIR /app
+
+COPY go.* ./
+RUN go mod download
+
+COPY . ./
+
+RUN go build -v -o server
+
+FROM debian:buster-slim
+RUN set -x && apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  ca-certificates && \
+  rm -rf /var/lib/apt/lists/*
+
+COPY --from=builder /app/server /app/server
 
 ENV PORT 80
 
@@ -7,13 +23,6 @@ EXPOSE 80
 EXPOSE 4000
 EXPOSE 8080
 
-WORKDIR /app
 
-# COPY go.mod ./
-# COPY go.sum ./
-# RUN go mod download
-
-COPY ./marriage-invitation-back-end .
-
-CMD ["./marriage-invitation-back-end"]
+CMD ["/app/server"]
 
--- main.go
+++ main.go
@@ -1,35 +1,24 @@
 package main
 
 import (
-<<<<<<< HEAD
-=======
 	"fmt"
->>>>>>> f50c4ca4fcf25027bd85fb88b821367b43edbf36
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-<<<<<<< HEAD
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-=======
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
-
-func getAll(w http.ResponseWriter, r *http.Request)  {
+func getAll(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	personList := getBlogs()
 	// w.Header().Set("Content-Type", "application/json")
@@ -45,11 +34,9 @@ func handleRequests() {
 	http.HandleFunc("/all", getAll)
 	http.HandleFunc("/", landing)
 	log.Fatal(http.ListenAndServe(":80", nil))
->>>>>>> f50c4ca4fcf25027bd85fb88b821367b43edbf36
 }
 
 func main() {
 	connectToDB()
 	handleRequests()
 }
-
